64a1557bf2695afc19575861a8095ed63e040af4
MarcusDLG <delag010@gmail.com>
2020-03-25 15:08:46 -0400

    Explorer Mode Complete

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,60 +8,66 @@ const HomePage = () => {
     <>
       <h1>My Favorite Bands</h1>
 
-      <section className="bandCard">
-        <img
-          className="bandPic"
-          src={Coheed}
-          alt="Coheed and Cambria Backstage"
-        ></img>
-        <section className="briefDescription">
-          <h1>Coheed and Cambria</h1>
-          <p>
-            Coheed is an American progressive rock band from Nyack, New York,
-            formed in 1995. Most of Coheed and Cambria's albums are concept
-            albums based on a science fiction storyline called The Amory Wars, a
-            series written by Claudio Sanchez, which has been transcribed into a
-            series of comic books as well as a full-length novel. The band has
-            released nine studio albums, three live albums, and several
-            special-edition releases.
-          </p>
+      <section className="Bands">
+        <section className="bandCard">
+          <img
+            className="bandPic"
+            src={Coheed}
+            alt="Coheed and Cambria Backstage"
+          ></img>
+          <section className="briefDescription">
+            <h1>Coheed and Cambria</h1>
+            <p>
+              Coheed is an American progressive rock band from Nyack, New York,
+              formed in 1995. Most of Coheed and Cambria's albums are concept
+              albums based on a science fiction storyline called The Amory Wars,
+              a series written by Claudio Sanchez, which has been transcribed
+              into a series of comic books as well as a full-length novel. The
+              band has released nine studio albums, three live albums, and
+              several special-edition releases.
+            </p>
+          </section>
         </section>
       </section>
-      <section className="bandCard">
-        <img
-          className="bandPic rdgldgrn"
-          src={RDGLDGRN}
-          alt="RedGoldGreen Banner"
-        ></img>
-        <section className="briefDescription">
-          <h1>RDGLDGRN</h1>
-          <p>
-            RDGLDGRN (pronounced Red Gold Green) is an American band based out
-            of Reston, Virginia, a suburb of Washington, D.C.. Formed in 2011,
-            the band recorded their debut album at Sound City Studios in 2012
-            with producer Kevin Augunas and engineer Clif Norrell. They have
-            worked with a wide range of notable musicians, including Dave Grohl
-            and Pharrell Williams.
-          </p>
+      <section className="Bands">
+        <section className="bandCard">
+          <img
+            className="bandPic rdgldgrn"
+            src={RDGLDGRN}
+            alt="RedGoldGreen Banner"
+          ></img>
+          <section className="briefDescription">
+            <h1>RDGLDGRN</h1>
+            <p>
+              RDGLDGRN (pronounced Red Gold Green) is an American band based out
+              of Reston, Virginia, a suburb of Washington, D.C.. Formed in 2011,
+              the band recorded their debut album at Sound City Studios in 2012
+              with producer Kevin Augunas and engineer Clif Norrell. They have
+              worked with a wide range of notable musicians, including Dave
+              Grohl and Pharrell Williams.
+            </p>
+          </section>
         </section>
       </section>
-      <section className="bandCard">
-        <img
-          className="bandPic billie"
-          src={Billie}
-          alt="Billie Eilish Album Cover"
-        ></img>
-        <section className="briefDescription">
-          <h1>Billie Eilish</h1>
-          <p>
-            Coheed is an American progressive rock band from Nyack, New York,
-            formed in 1995. Most of Coheed and Cambria's albums are concept
-            albums based on a science fiction storyline called The Amory Wars, a
-            series written by Claudio Sanchez, which has been transcribed into a
-            series of comic books as well as a full-length novel. The band has
-            released nine studio albums, three live albums, and several
-            special-edition releases.
-          </p>
+      <section className="Bands">
+        <section className="bandCard">
+          <img
+            className="bandPic billie"
+            src={Billie}
+            alt="Billie Eilish Album Cover"
+          ></img>
+          <section className="briefDescription">
+            <h1>Billie Eilish</h1>
+            <p>
+              Coheed is an American progressive rock band from Nyack, New York,
+              formed in 1995. Most of Coheed and Cambria's albums are concept
+              albums based on a science fiction storyline called The Amory Wars,
+              a series written by Claudio Sanchez, which has been transcribed
+              into a series of comic books as well as a full-length novel. The
+              band has released nine studio albums, three live albums, and
+              several special-edition releases.
+            </p>
+          </section>
         </section>
       </section>
     </>
